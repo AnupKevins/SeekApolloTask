ce714c4ae971fff1c5494378d9b5364d93f9800c
Anup <anup.sahu@thoughtworks.com>
2024-02-23 10:01:31 +0530

    Initial Commit

---
+++ SeekJobsTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeekJobsTask
+//
+//  Created by Anup kumar sahu on 23/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
